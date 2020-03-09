969ec6cce1550919d35954245691bf364ba23683
juel abreu <juelabreu@juels-MBP.fios-router.home>
2020-03-08 22:56:49 -0400

    first commit

---
+++ Desktop/Time Traveler/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/GameData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static int lives = 3;
+    public static int level = 1;
+
+    public static bool isWin = false;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public static void reset()
+    {
+        lives = 3;
+        level = 1;
+
+        isWin = false;
+    }
+}
